67af4e29f9cc3ffb2bd8b9bf3353fb4042532766
zzhh <691027259@qq.com>
2022-06-27 09:23:37 +0800

    inti

---
+++ src/test/java/com/internship/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
